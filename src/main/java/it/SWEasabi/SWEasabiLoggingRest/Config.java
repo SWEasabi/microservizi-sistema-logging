ce7cb591341385acb0228d9ad1d088f7b8ea5c3a
bonni1207 <44138357+bonni1207@users.noreply.github.com>
2023-07-30 19:21:26 +0200

    Tolti altri riferimenti inutilizzati

--- src/main/java/it/SWEasabi/SWEasabiLoggingRest/Config.java
+++ src/main/java/it/SWEasabi/SWEasabiLoggingRest/Config.java
@@ -1,14 +1,12 @@
 package it.SWEasabi.SWEasabiLoggingRest;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import it.SWEasabi.logging.CoreLoggingService;
 import it.SWEasabi.logging.services.DatabaseReaderService;
 import it.SWEasabi.logging.services.JsonBuilderService;
 import it.SWEasabi.logging.services.LocalJsonBuilderService;
-import it.SWEasabi.logging.services.LocalReaderService;
 import it.SWEasabi.logging.services.ReaderService;
 
 @Configuration
